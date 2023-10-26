79d1797e25b5aded6ef8e34b204dff68c3dc2ae7
McleodHerritt <nmcleod2021@gmail.com>
2023-10-26 13:26:15 -0300

    Updated user prompt to provide a list of options for viewing and updating the database.

--- CMS.js
+++ CMS.js
@@ -4,16 +4,58 @@ class CMS {
   constructor() {}
 
   run() {
+    return this.promptMainMenu();
+  }
+
+  promptMainMenu() {
     return inquirer
       .prompt([
         {
-          type: "input",
-          name: "test",
-          message: "inquirer test:",
+          type: "list",
+          name: "action",
+          message: "What would you like to do?",
+          choices: [
+            "View all departments",
+            "View all roles",
+            "View all employees",
+            "Add a department",
+            "Add a role",
+            "Add an employee",
+            "Update an employee role",
+            "Exit",
+          ],
         },
       ])
       .then((answers) => {
-        console.log("entered " + answers.test);
+        switch (answers.action) {
+          case "View all departments":
+            // Logic for viewing all departments
+            break;
+          case "View all roles":
+            // Logic for viewing all roles
+            break;
+          case "View all employees":
+            // Logic for viewing all employees
+            break;
+          case "Add a department":
+            // Logic for adding a department
+            break;
+          case "Add a role":
+            // Logic for adding a role
+            break;
+          case "Add an employee":
+            // Logic for adding an employee
+            break;
+          case "Update an employee role":
+            // Logic for updating an employee role
+            break;
+          case "Exit":
+            // Exit the application
+            process.exit(0);
+            break;
+        }
+        // After an action is completed, show the menu again
+        return this.promptMainMenu();
       });
   }
 }
